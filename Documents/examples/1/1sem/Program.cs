54d265817639992a534cf8360367eef013bc43ca
Савченко Александр Владимирович <usunnyblin@gmail.com>
2022-09-18 21:39:29 +0300

    +1sem

---
+++ Documents/examples/1/1sem/Program.cs
@@ -0,0 +1,60 @@
+﻿// Квадрат числа
+//Console.WriteLine ("Введите число");
+//int a = Convert.ToInt32(Console.ReadLine());
+//int b = a * a;
+//Console.WriteLine ($"Результат: {b}");
+//Console.WriteLine ($"{a} * {a} = {b}");
+
+//Счётичик while мультики
+//int time = 11;
+
+// while (time <22)
+// {
+//   time++;
+//   Console.WriteLine("ещё можно смотреть мультитки. время:" + time); 
+// }
+// Console.WriteLine("пора спать. время: " + time); 
+
+
+// //Квадрат ли число?
+// Console.Write("Введите сумму ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите множимое ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a == b * b)
+//     Console.WriteLine("ДА");
+// else 
+//     Console.WriteLine("НЕТ");
+
+
+// //Вложенные циклы + неверный ввод
+// if (a == 1)
+
+// else if (a == 2); 
+
+// else 
+//   Console.WriteLine("Неверный ввод");
+
+
+// функция и параметр Возведение в квадрат
+// int Power (int x)
+// {
+//   return x * x;
+// }
+// Console.Write("Введите множимое ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Power(a));
+
+
+//все целые числа от -N до N 
+// Console.Write("Введите множимое ");
+// int num1 =  Convert.ToInt32(Console.ReadLine());
+// num1 = Math.Abs(num1);
+// int num2 = num1 * -1;
+
+// while (num2 <= num1)
+// {  
+//   Console.Write(num2 + ", ");
+//   num2++;
+// }
+// Console.WriteLine();
