fe7f871d63175968f71ab5ee70e44bed49e2a2f1
Савченко Александр Владимирович <usunnyblin@gmail.com>
2022-11-14 19:01:48 +0300

    2 step

--- Documents/examples/1/Kurs/Program.cs
+++ Documents/examples/1/Kurs/Program.cs
@@ -1,7 +1,10 @@
 ﻿
-//Итоговая задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
+//Итоговая задача 
+
+//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа. 
 //Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 //лучше обойтись исключительно массивами.
+
 //Примеры:
 //["Hello",  "2” , "World" , "=)" -> ["2" , "=)"]
 //["1234", "1567", "-2", "сошри*ег зшепсе"] -> ["-2"]
@@ -12,7 +15,45 @@
 void Zadacha()
 {
 
+{
+   //Random random = new Random();
+    Console.WriteLine($"Введите сколько будет значений: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    string[] array = new string[size];
+
+    // Заполнять с клавы
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}-й элемент массива: ");
+        array[i] = Console.ReadLine();
+    }
+    PrintArray(array);
+    PrintArray1(array);
 }
 
+// Печатать массив
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
 
-Zadacha();
+//Заполняем и печатаем новый массив
+void PrintArray1(string[] array)
+{
+    Console.WriteLine("Вывод массива: ");
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        int sluchai = random.Next(0, 2);
+        if (sluchai != 0) Console.Write(array[i] + " ");
+    }
+    Console.Write(" ");
+    Console.WriteLine();
+}
+
+}
