edf861338a846ffc25101a36433cb2fd63cc7607
sunnyzlo <98699207+sunnyzlo@users.noreply.github.com>
2023-01-29 22:02:37 +0300

    Update DZ1.java

--- Documents/examples/Java/DZ1/DZ1.java
+++ Documents/examples/Java/DZ1/DZ1.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 public class lesson1 {
     
     public static void main(String[] args){
-дз 1 
+//дз 1 
     //1. Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n) тр 5 = 1+2+3+4+5
 
 
